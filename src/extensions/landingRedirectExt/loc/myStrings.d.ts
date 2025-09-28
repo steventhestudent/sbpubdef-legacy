4c46daeea1b49f2f6c73503034ccb23c221f5620
sp_setup <sp_setup@lab.local>
2025-09-28 14:52:11 -0700

    scaffold ~/sbpubdef, sbpubdef-sol, landing-redirect-ext (ApplicationCustomizer)

---
+++ src/extensions/landingRedirectExt/loc/myStrings.d.ts
@@ -0,0 +1,8 @@
+declare interface ILandingRedirectExtApplicationCustomizerStrings {
+  Title: string;
+}
+
+declare module 'LandingRedirectExtApplicationCustomizerStrings' {
+  const strings: ILandingRedirectExtApplicationCustomizerStrings;
+  export = strings;
+}
