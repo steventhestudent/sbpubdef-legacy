e31b450d0aa0296662b2f7e9d2ec6efc29d9cf18
sp_setup <sp_setup@lab.local>
2025-09-28 18:30:25 -0700

    init

--- src/extensions/landingRedirectExt/loc/myStrings.d.ts
+++
@@ -1,8 +0,0 @@
-declare interface ILandingRedirectExtApplicationCustomizerStrings {
-  Title: string;
-}
-
-declare module 'LandingRedirectExtApplicationCustomizerStrings' {
-  const strings: ILandingRedirectExtApplicationCustomizerStrings;
-  export = strings;
-}
---
+++ src/webparts/landingRedirectWebpart/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ILandingRedirectWebpartWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'LandingRedirectWebpartWebPartStrings' {
+  const strings: ILandingRedirectWebpartWebPartStrings;
+  export = strings;
+}
