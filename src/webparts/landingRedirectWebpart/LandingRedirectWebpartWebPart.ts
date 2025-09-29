d0cb8e137f009c3c70c4a58cc189e0bff1c5005b
sp_setup <sp_setup@lab.local>
2025-09-28 20:17:07 -0700

    landing-redirect works (not role-based yet)

--- src/webparts/landingRedirectWebpart/LandingRedirectWebpartWebPart.ts
+++ src/webparts/landingRedirectWebpart/LandingRedirectWebpartWebPart.ts
@@ -16,21 +16,45 @@ export interface ILandingRedirectWebpartWebPartProps {
 export default class LandingRedirectWebpartWebPart extends BaseClientSideWebPart<ILandingRedirectWebpartWebPartProps> {
 
   public render(): void {
+    let redirectionCancelled = false;
+
+    function performRedirection() {
+        location.pathname = '/sites/Attorney';
+        console.log('perform redirect ');
+    }
+
+    const delayedRedirection = () => 
+      setTimeout(() => {
+        if (redirectionCancelled) return console.log('redir canceled');
+        performRedirection();
+      }, 333 * 4);
+
+    window.onkeydown = (e: KeyboardEvent) => {
+      if (e.key === 'Escape') redirectionCancelled = true;
+    };
+    window.onkeyup = (e: KeyboardEvent) => {
+      if (e.key === 'Escape') redirectionCancelled = true;
+    };
+
+    // if (location.pathname.toLowerCase().indexOf('/sites/pd-internal') === 0)
+      delayedRedirection();
+    // else console.log('landing-redirect-webpart: no redirection (not on /sites/PD-Internal)');
+
     this.domElement.innerHTML = `
       <div class="${ styles.landingRedirectWebpart }">
         <div class="${ styles.container }">
           <div class="${ styles.row }">
             <div class="${ styles.column }">
-              <span class="${ styles.title }">Welcome to SharePoint!</span>
-              <p class="${ styles.subTitle }">Customize SharePoint experiences using Web Parts.</p>
-              <p class="${ styles.description }">${escape(this.properties.description)}</p>
-              <a href="https://aka.ms/spfx" class="${ styles.button }">
-                <span class="${ styles.label }">Learn more</span>
-              </a>
+              <span class="${ styles.title }">Redirecting to: /sites/&lt;role&gt;</span>
+              <p class="${ styles.subTitle }">A role-based redirection.</p>
+              <p class="${ styles.description }">${escape(this.properties.description)} (description).</p>
+              <a href="/" class="${ styles.button }"><span class="${ styles.label }">Redirect Now</span></a>
+              <a href="#" class="${ styles.button }"><span class="${ styles.label }">Stay Here</span></a>
             </div>
           </div>
         </div>
-      </div>`;
+      </div>
+      `;
   }
 
   protected get dataVersion(): Version {
